PHPMailer/PHPMailer

commit 7932f2de8c8a3fa05c03c9c97d2dd9dccd332ab7

Standards

src/PHPMailer.php
@@ -3052,8 +3052,9 @@ class PHPMailer
      * @param string $str      The text to encode
      * @param string $encoding The encoding to use; one of 'base64', '7bit', '8bit', 'binary', 'quoted-printable'
      *
-     * @return string
      * @throws Exception
+     *
+     * @return string
      */
     public function encodeString($str, $encoding = self::ENCODING_BASE64)
     {
@@ -3353,7 +3354,7 @@ class PHPMailer
             $this->attachment[] = [
                 0 => $string,
                 1 => $filename,
-                2 => basename($filename),
+                2 => static::mb_pathinfo($filename, PATHINFO_EXTENSION),
                 3 => $encoding,
                 4 => $type,
                 5 => true, // isStringAttachment

upmorpheus/PHP-Mailer

commit 1be9e538141804a8ee4a665273d2a0f028cbaa6d

Fix for #1753. This bug was introduced in 7932f2de8c8a3fa05c03c9c97d2dd9dccd332ab7, so was never in a tagged release.

src/PHPMailer.php
@@ -3354,7 +3354,7 @@ class PHPMailer
             $this->attachment[] = [
                 0 => $string,
                 1 => $filename,
-                2 => static::mb_pathinfo($filename, PATHINFO_EXTENSION),
+                2 => static::mb_pathinfo($filename, PATHINFO_BASENAME),
                 3 => $encoding,
                 4 => $type,
                 5 => true, // isStringAttachment